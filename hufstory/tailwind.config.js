652ef89e26e019aed0581319c4d721c592d851ce
DongHyeok Lim <zx07050@naver.com>
2022-11-30 02:13:02 +0900

    Chore: Update

--- hufstory/tailwind.config.js
+++ hufstory/tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
-  darkMode: false, // or 'media' or 'class'
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-
-  variants: {
-    extends: {},
+  theme: {
+    extend: {},
   },
   plugins: [],
 }
