6e74cf353e974e81db62257da935a1a027e85825
DongHyeok Lim <zx07050@naver.com>
2022-12-23 19:58:59 +0900

    Style: 디자인 재구상을 위한 border 제거

--- hufstory/src/components/Header.tsx
+++ hufstory/src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 
 const Header = () => {
   return (
-    <header className="sticky inset-x-0 top-0 z-50 left-0 bg-white text-gray-700 body-font border-b border-gray-200">
+    <header className="sticky inset-x-0 top-0 z-50 left-0 bg-white text-gray-700 body-font">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <Link
           className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
