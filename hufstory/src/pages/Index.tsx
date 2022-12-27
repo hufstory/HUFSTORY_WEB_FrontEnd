ea07d99f88b1b131337966d212fc92e146bb48c4
DongHyeok Lim <zx07050@naver.com>
2022-12-27 17:34:56 +0900

    Docs: Index

--- hufstory/src/pages/Index.tsx
+++ hufstory/src/pages/Index.tsx
@@ -1,16 +1,18 @@
 import React from "react"
 
 import Header from "../components/Header"
+import Main from "../components/Main"
 import Footer from "../components/Footer"
 
-const Main = () => {
+// Main Page -> 컴포넌트 최소한으로 깔끔하게 만들 것
+const Index = () => {
   return (
     <div>
       <Header />
-      Hufstory Main Page
+      <Main />
       <Footer />
     </div>
   )
 }
 
-export default Main
+export default Index
