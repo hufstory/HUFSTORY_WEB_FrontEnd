a4abf1a4d4a42ad75cafedb8ab2aa3157b0dd73e
DongHyeok Lim <zx07050@naver.com>
2022-12-31 17:46:04 +0900

    Style: Main Beta

--- hufstory/src/pages/Main.tsx
+++ hufstory/src/pages/Main.tsx
@@ -11,6 +11,8 @@ const Main = () => {
       <Header />
 
       <div className="ml-36 mr-36 mt-8 md:px-5 md:py-5 md:flex-row items-center">
+
+        {/*
         <div className="inline-block ml-10 hover:-translate-y-1 hover:scale-100 duration-300">
           <img src={hufstoryLogo} alt="MainHufstory"></img>
         </div>
@@ -20,15 +22,16 @@ const Main = () => {
         </div>
 
         <hr className="h-px bg-gray-200"></hr>
+        */}
 
-        <div className="flex mt-20 text-black body-font bg-transparent relative p-10 rounded-[14px] shadow-default hover:-translate-y-1 hover:scale-100 duration-300">
+        <div className="flex mt-10 mb-10 text-black body-font bg-transparent relative p-10 rounded-[14px] shadow-default hover:-translate-y-1 hover:scale-100 duration-300">
           <div className="mt-8 w-[32rem] block">
             <h1 className="block w-80 font-bold text-3xl mb-5">
               한국외국어대학교 <br></br>학생 IT 자치기구 훕스토리
             </h1>
             <br></br>
             <h2 className="font-bold text-lg">훕스토리란?</h2>
-            <p className="mt-2 mb-4">
+            <p className="mt-1 mb-6">
               개발, 기획&디자인 팀으로 이루어진 교내 학생 자치기구입니다. 개발팀
               & 기획 및 디자인 팀으로 구성되어 외대생을 비롯한 대학생들을 위해
               SW 서비스 개발 및 행사 기획을 진행합니다. 자유로운 의사소통과 팀원
@@ -36,13 +39,13 @@ const Main = () => {
             </p>
 
             <h2 className="font-bold text-lg">개발(Developer)</h2>
-            <p className="mt-2 mb-2">
+            <p className="mt-1 mb-6">
               학부 과정에서는 쉽게 경험할 수 없는 다양한 기술을 사용하며
               학부생의 실력에 그치지 않는 팀을 지향하고 있습니다.
             </p>
 
             <h2 className="font-bold text-lg">기획 & 디자인</h2>
-            <p className="mt-2 mb-4">
+            <p className="mt-1 mb-6">
               주로 컨텐츠, 기획, 디자인을 하며 창의적인 활동을 지향하며 개발
               파트를 맡은 팀원들과 함께 프로젝트를 진행합니다. 개발을 하며
               프로젝트를 진행하며 IT 기획, 프로젝트에 필요한 디자인을 구상하고
@@ -51,7 +54,7 @@ const Main = () => {
             </p>
 
             <h1 className="font-bold text-lg">무엇을 할 수 있을까?</h1>
-            <p className="mt-2 mb-4">
+            <p className="mt-1 mb-6">
               "훕스토리"는 좋은 사람들과 좋은 프로그램이 함께 합니다. 다양한
               프로젝트 경험을 쌓을 수 있고, 멘토링을 통하여 다양한 기술을 학습할
               수 있습니다. 본인이 원할 시 다대일 멘토링을 통하여 다양한 기술을
