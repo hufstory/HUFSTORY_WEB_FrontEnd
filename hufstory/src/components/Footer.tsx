38d414b56aadaa142f8be0785bfadd20c4c1ad48
DongHyeok Lim <zx07050@naver.com>
2022-12-31 17:45:19 +0900

    Rename: 상수 변수명 수정

--- hufstory/src/components/Footer.tsx
+++ hufstory/src/components/Footer.tsx
@@ -1,15 +1,15 @@
 import React from "react"
 
 const Footer = () => {
-  const currentDay = new Date()
-  const currentYear = currentDay.getFullYear()
+  const CURRENT_DAY = new Date()
+  const CURRENT_YEAR = CURRENT_DAY.getFullYear()
 
   return (
     <footer className="text-gray-700 body-font">
       <div className="bg-gray-200">
         <div className="container mx-auto pt-4 px-5 flex flex-wrap flex-col sm:flex-row">
           <p className="text-gray-500 text-sm text-center sm:text-left">
-            © {currentYear} Hufstory
+            © {CURRENT_YEAR} Hufstory
           </p>
           <span className="sm:ml-auto sm:mt-0 mt-2 sm:w-auto w-full sm:text-left text-center text-gray-500 text-sm">
             경기도 용인시 처인구 모현읍 외대로 81, 한국외국어대학교 글로벌캠퍼스
@@ -18,7 +18,7 @@ const Footer = () => {
         </div>
         <div className="container mx-auto pb-4 px-5 flex flex-wrap flex-col sm:flex-row">
           <span className="sm:ml-auto sm:mt-0 mt-2 sm:w-auto w-full sm:text-left text-center text-gray-500 text-sm">
-            Developed by
+            Developed by 임동혁
           </span>
         </div>
       </div>
