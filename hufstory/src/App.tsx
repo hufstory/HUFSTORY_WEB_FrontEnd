8a50d5cd006d72d2a781204cc939aca874483f2e
DongHyeok Lim <zx07050@naver.com>
2023-01-04 18:06:34 +0900

    Docs: 임시적으로 Main 페이지 변경

--- hufstory/src/App.tsx
+++ hufstory/src/App.tsx
@@ -1,17 +1,18 @@
 import React from "react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Main from "./pages/Main"
-import About from "./pages/About"
-import Teams from "./pages/Teams"
-import Recruit from "./pages/Recruit"
-import Blog from "./pages/Blog"
+import Main from "./pages/Main/Main"
+import RealMain from "./pages/Main/RealMain"
+import About from "./pages/About/About"
+import Teams from "./pages/Teams/Teams"
+import Recruit from "./pages/Recruit/Recruit"
+import Blog from "./pages/Blog/Blog"
 
 // 모든 컴포넌트 전부 호출 (화면 하나하나 전부 다)
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/" element={<RealMain />} />
         <Route path="/about" element={<About />} />
         <Route path="/teams" element={<Teams />} />
         <Route path="/recruit" element={<Recruit />} />
