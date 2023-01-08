d8f0ee02ef3f49481110f33f76b08b2fe120efa0
DongHyeok Lim <zx07050@naver.com>
2023-01-08 18:49:18 +0900

    Style: CSS

--- hufstory/tailwind.config.js
+++ hufstory/tailwind.config.js
@@ -4,58 +4,90 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      backgroundImage: {
+        title: "url('../../static/images/logo/titleLogo2.png')",
+      },
+
       keyframes: {
+        appear: {
+          "0%": {
+            opacity: "0",
+          },
+          "100%": {
+            opacity: "1",
+          },
+        },
+
         childMove: {
           "0%": {
             opacity: "0.5",
-            transform: "translateX(-50%) translateY(-50%)",
+            transform: "translateX(-50%) translateY(-100%)",
           },
           "75%": {
-            transform: "translateX(2%) translateY(2%)",
+            transform: "translateX(3%) translateY(3%)",
           },
-          "100%": { opacity: "1", transform: "translateY(0)" },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
         },
+
         catMove: {
           "0%": {
-            opacity: "0.5",
+            opacity: "1",
             transform: "translateX(50%)",
           },
           "75%": {
-            transform: "translateX(-2%)",
+            transform: "translateX(-3%)",
           },
-          "100%": { opacity: "1", transform: "translateX(0)" },
+          "100%": {
+            opacity: "1",
+            transform: "translateX(0)",
+          },
         },
+
         greenMove: {
           "0%": {
-            opacity: "0.5",
+            opacity: "1",
             transform: "translateX(50%) translateY(50%)",
           },
           "75%": {
-            transform: "translateX(-2%) translateY(-2%)",
+            transform: "translateX(-3%) translateY(-3%)",
           },
-          "100%": { opacity: "1", transform: "translateY(0)" },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
         },
+
         titleMove: {
           "0%": {
             opacity: "0",
-            transform: "translateX(100%)",
+            transform: "translateX(300%)",
           },
           "75%": {
-            transform: "translateX(-2%) translateY(-2%)",
+            transform: "translateX(-3%) translateY(-3%)",
           },
-          "100%": { opacity: "1", transform: "translateX(0)" },
+          "100%": {
+            opacity: "1",
+            transform: "translateX(0)",
+          },
         },
       },
+
       animation: {
-        childMove: "childMove 1s ease-in-out",
-        catMove: "catMove 1s ease-in-out",
-        greenMove: "greenMove 1s ease-in-out",
-        titleMove: "titleMove 1s ease-in-out",
+        appear: "appear 1s ease-in-out",
+        childMove: "childMove 0.7s ease-in-out",
+        catMove: "catMove 0.7s ease-in-out",
+        greenMove: "greenMove 0.7s ease-in-out",
+        titleMove: "titleMove 1s ease-in-out 1500ms",
       },
+
       colors: {
         "represent-color": "#A1CCFB",
         "title-color": "#3DA3FB",
       },
+
       boxShadow: {
         default: "4px 12px 30px 6px rgba(0, 0, 0, 0.09)",
       },
