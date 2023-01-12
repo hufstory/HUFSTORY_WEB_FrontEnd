f253561c6f1aefdda67f3c08020662b679b02d8e
DongHyeok Lim <zx07050@naver.com>
2023-01-12 18:34:23 +0900

    Docs: Activity Category

--- hufstory/src/App.tsx
+++ hufstory/src/App.tsx
@@ -9,6 +9,7 @@ import Teams_Gbus from "./pages/Teams/Teams_Gbus"
 import Teams_Hubigo from "./pages/Teams/Teams_Hubigo"
 import Recruit from "./pages/Recruit/Recruit"
 import Blog from "./pages/Blog/Blog"
+import Activity from "./pages/Activity/Activity"
 
 // 모든 컴포넌트 전부 호출 (화면 하나하나 전부 다)
 function App() {
@@ -17,6 +18,7 @@ function App() {
       <Routes>
         <Route path="/" element={<RealMain />} />
         <Route path="/about" element={<About />} />
+        <Route path="/activity" element={<Activity />} />
         <Route path="/teams" element={<Teams />} />
         <Route path="/teams_oechul" element={<Teams_Oechul />} />
         <Route path="/teams_gbus" element={<Teams_Gbus />} />
