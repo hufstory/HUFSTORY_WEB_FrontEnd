f844104c72f54bcb5daf9d3d0950b3ae11f5328f
DongHyeok Lim <zx07050@naver.com>
2023-01-12 18:38:12 +0900

    Refactor: Responsive Code
    반응형으로 변경

--- hufstory/src/pages/Teams/Teams.tsx
+++ hufstory/src/pages/Teams/Teams.tsx
@@ -4,11 +4,12 @@ import Header from "../../components/Header"
 import Footer from "../../components/Footer"
 import { Link } from "react-router-dom"
 
+// 첫 화면부터 바로 훕포메이션으로 시작
 const Teams = () => {
   return (
     <div>
       <Header />
-      <div className="ml-36 mr-36 mt-8 md:px-5 md:py-5 md:flex-row text-center items-center font-nanumRegular">
+      <div className="w-4/5 mx-auto mt-8 md:px-5 md:py-5 md:flex-row text-center items-center font-nanumRegular">
         <div className="flex flex-col mt-10 mb-10 text-black body-font bg-transparent relative p-10 rounded-[14px] shadow-default hover:-translate-y-1 hover:scale-100 duration-300">
           <h2 className="text-xs text-sky-500 tracking-widest font-medium title-font mb-1 font-nanumRegular">
             Teams
@@ -16,37 +17,39 @@ const Teams = () => {
           <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900 font-nanumBold">
             2022 - 현재 진행 팀
           </h1>
-          <div className="flex flex-wrap lg:flex lg:flex-row place-content-center mt-8">
+          <div className="flex flex-wrap lg:flex-row place-content-center mt-8">
             <Link to="/Teams">
-              <button className="text-gray-600 bg-white border-b-2 border-sky-400 py-2 px-8 hover:text-sky-600 hover:border-sky-600 text-lg">
+              <button className="text-gray-600 bg-white border-b-2 border-sky-400 py-2 px-8 hover:text-sky-600 hover:border-sky-600 text-sm sm:text-base md:text-lg">
                 훕포메이션
               </button>
             </Link>
             <Link to="/Teams_Oechul">
-              <button className="text-gray-600 bg-white border-b-2 py-2 px-8 hover:text-sky-600 hover:border-sky-600 text-lg">
+              <button className="text-gray-600 bg-white border-b-2 py-2 px-8 hover:text-sky-600 hover:border-sky-600 text-sm sm:text-base md:text-lg">
                 외출: 외대생 솔로 탈출 프로젝트
               </button>
             </Link>
             <Link to="/Teams_Gbus">
-              <button className="text-gray-600 bg-white border-b-2 py-2 px-8 hover:text-sky-600 hover:border-sky-600 text-lg">
+              <button className="text-gray-600 bg-white border-b-2 py-2 px-8 hover:text-sky-600 hover:border-sky-600 text-sm sm:text-base md:text-lg">
                 G버스: 버스 위치 정보 서비스
               </button>
             </Link>
             <Link to="/Teams_Hubigo">
-              <button className="text-gray-600 bg-white border-b-2 py-2 px-8 hover:text-sky-600 hover:border-sky-600 text-lg">
+              <button className="text-gray-600 bg-white border-b-2 py-2 px-8 hover:text-sky-600 hover:border-sky-600 text-sm sm:text-base md:text-lg">
                 HUBIGO: 강의 평가 서비스
               </button>
             </Link>
           </div>
 
           <div className="mt-8">
-            <h1 className="text-2xl mb-2">훕포메이션 한국외대 챗봇 서비스</h1>
-            <p>
+            <h1 className="text-xl sm:text-2xl mb-4">
+              훕포메이션 한국외대 챗봇 서비스
+            </h1>
+            <p className="mb-2 text-sm sm:text-base">
               훕포메이션은 한국외대 학식 정보를 카카오톡으로 간편하게 조회할 수
               있는 플러스친구 챗봇입니다.
             </p>
 
-            <p>
+            <p className="text-sm sm:text-base">
               2017년 5월부터 개인 프로젝트로 제품 기획/개발이 진행되었습니다.
               <br></br>
               출시 당시: 기존에 인문대 학생들의 ‘외대세끼’라는 플러스친구 기반의
