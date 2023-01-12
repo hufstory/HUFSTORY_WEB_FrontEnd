7edaf76eba3bc6e224fe9e52ae3a5992b0145ac7
DongHyeok Lim <zx07050@naver.com>
2023-01-12 18:34:43 +0900

    Style: Blog 상태 수정

--- hufstory/src/pages/Blog/Blog.tsx
+++ hufstory/src/pages/Blog/Blog.tsx
@@ -8,7 +8,16 @@ const Blog = () => {
     <div>
       <Header />
       <div className="ml-36 mr-36 mt-8 md:px-5 md:py-5 md:flex-row items-center">
-        <h1>Blog</h1>
+        <div className="flex mt-10 mb-10 text-black body-font bg-transparent relative p-10 rounded-[14px] shadow-default hover:-translate-y-1 hover:scale-100 duration-300">
+          <div className="mt-4 block">
+            <h1 className="block font-bold text-3xl mb-5 font-nanumBold">
+              Blog
+            </h1>
+            <p className="mx-auto mb-6 font-nanumRegular">
+              Blog 미구현 상태입니다.
+            </p>
+          </div>
+        </div>
       </div>
       <Footer />
     </div>
